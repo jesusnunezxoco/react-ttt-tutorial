8a73ba7e633de8b6e7ca2a1501a4a7e590fe4b27
Jesus Nunez Xoconoxtle <jesusnunezxoco@gmail.com>
2021-12-30 08:38:49 -0600

    seperated components into files

---
+++ app/src/Game.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Board from "./components/Board";
+
+// Parent comopnent - equivalent to App.js in class
+export default function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
+  );
+}
