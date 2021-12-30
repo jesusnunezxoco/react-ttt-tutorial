04d652db36db09429abd1cb8e13a2c07d59ee9c1
Jesus Nunez Xoconoxtle <jesusnunezxoco@gmail.com>
2021-12-30 09:07:04 -0600

    added click function to buttons

--- app/src/components/Square.js
+++ app/src/components/Square.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 export default class Square extends React.Component {
+
+  handleClick = () => {
+    console.log("clickkity click")
+  }
+
   render() {
     return (
       // square will access the `value` prop, which will be a number, through `this.props`
-      <button className="square">
+      <button className="square" onClick={this.handleClick}>
         {this.props.value}
       </button>
     );
