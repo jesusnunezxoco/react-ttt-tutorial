ace78961ad343a264b06667acfbaf783f7a60639
Jesus Nunez Xoconoxtle <jesusnunezxoco@gmail.com>
2021-12-30 08:54:46 -0600

    broke up components into seperate files

---
+++ app/src/Game.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Board from "./components/Board";
+
+// parent component similar to App.js in our previous projects
+
+export default class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
