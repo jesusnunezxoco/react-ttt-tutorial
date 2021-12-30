85a1032ef306ad1e1cf152fa2b3d32be0905dd17
Jesus Nunez Xoconoxtle <jesusnunezxoco@gmail.com>
2021-12-30 09:16:03 -0600

    added click event

--- app/src/components/Square.js
+++ app/src/components/Square.js
@@ -2,6 +2,12 @@ import React from "react";
 
 // need to have props as a parameter for the functional component
 export default function Square(props) {
+
+    const handleClick = () => {
+      console.log("clickity click")
+    }
+
+
   // will access value through the `props` object
-  return <button className="square">{props.value}</button>;
+  return <button className="square" onClick={handleClick}>{props.value}</button>;
 }
