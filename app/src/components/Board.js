16c61c3a1f411638469fdd572e95fdfbcd3b66d0
Jesus Nunez Xoconoxtle <jesusnunezxoco@gmail.com>
2022-01-01 14:01:36 -0600

    updated state to display current player

--- app/src/components/Board.js
+++ app/src/components/Board.js
@@ -5,19 +5,24 @@ export default class Board extends React.Component {
   // state is in the Board component to easily share state accross its children
   constructor(props) {
     super(props);
-    this.state = { squares: Array(9).fill(null) };
+    this.state = { squares: Array(9).fill(null), xIsNext: true };
   }
-
-
 
   handleClick = (i) => {
     console.log("clicked square #", i);
     // make copy of squares state
-    let copy = this.state.squares.slice()
+    let copy = this.state.squares.slice();
     // update the copy
-    copy[i] = "X"
-    this.setState({squares: copy})
-    
+    // if xIsNext is true, then return string "X"
+    // else return string "O"
+    copy[i] = this.state.xIsNext ? "X" : "O";
+
+    this.setState({
+      // will replace squares with the updated copy
+      squares: copy,
+      // will flip who is next
+      xIsNext: !this.state.xIsNext,
+    });
   };
 
   // function called renderSquare that will take a number, `i`
@@ -25,14 +30,15 @@ export default class Board extends React.Component {
     // created a prop called `value`
     // the `i` parameter gets passed to `Square` through the `value` prop
     return (
-      <Square 
-      value={this.state.squares[i]}
-      handleClick ={() => this.handleClick(i)}  />
-    )
+      <Square
+        value={this.state.squares[i]}
+        handleClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
-    const status = "Next player: X";
+    const status = `Next player is: ${this.state.xIsNext ? "X" : "O"}`;
 
     return (
       <div>
@@ -56,3 +62,4 @@ export default class Board extends React.Component {
     );
   }
 }
+
