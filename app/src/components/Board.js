15aeb55d7b758230200aeb9fd01f865d5802e7e4
Jesus Nunez Xoconoxtle <jesusnunezxoco@gmail.com>
2022-01-01 14:16:35 -0600

    winnner calculator used

--- app/src/components/Board.js
+++ app/src/components/Board.js
@@ -38,7 +38,16 @@ export default class Board extends React.Component {
   }
 
   render() {
-    const status = `Next player is: ${this.state.xIsNext ? "X" : "O"}`;
+    const winner = calculateWinner(this.state.squares)
+    let status;
+    // if a winner has been determined
+    if(winner) {
+      // stop game, and display who the winner is
+      status = `Winner: ${winner}`
+    } else {
+      // otherwise,  display the next player
+      status = `Next player:${this.state.xIsNext ? "X" : "O"}`
+    }
 
     return (
       <div>
@@ -63,3 +72,22 @@ export default class Board extends React.Component {
   }
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
