971d7eb899475e35ff9edd6a5a8cfdcc4ad0bf60
Jesus Nunez Xoconoxtle <jesusnunezxoco@gmail.com>
2022-01-01 13:44:48 -0600

    handleClick created; pass down to Square and called there to update state

--- app/src/components/Board.js
+++ app/src/components/Board.js
@@ -2,18 +2,33 @@ import React from "react";
 import Square from "./Square";
 
 export default class Board extends React.Component {
+  // state is in the Board component to easily share state accross its children
   constructor(props) {
     super(props);
     this.state = { squares: Array(9).fill(null) };
   }
 
-  
+
+
+  handleClick = (i) => {
+    console.log("clicked square #", i);
+    // make copy of squares state
+    let copy = this.state.squares.slice()
+    // update the copy
+    copy[i] = "X"
+    this.setState({squares: copy})
+    
+  };
 
   // function called renderSquare that will take a number, `i`
   renderSquare(i) {
     // created a prop called `value`
-    // the `i` paramete gets passed to `Square` through the `value` prop
-    return <Square value={this.state.squares[i]}  />;
+    // the `i` parameter gets passed to `Square` through the `value` prop
+    return (
+      <Square 
+      value={this.state.squares[i]}
+      handleClick ={() => this.handleClick(i)}  />
+    )
   }
 
   render() {
--- app/src/components/Square.js
+++ app/src/components/Square.js
@@ -1,21 +1,13 @@
 import React from "react";
 
 export default class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: null };
-  }
-
-  handleClick = () => {
-    console.log("clickkity click");
-    this.setState({value: "X"})
-  };
-
   render() {
     return (
       // square will access the `value` prop, which will be a number, through `this.props`
-      <button className="square" onClick={this.handleClick}>
-        {this.state.value}
+      // it will use handleClick to update the state in the parent component'
+      // this is how you get child components pass up information, using functions
+      <button className="square" onClick={() => this.props.handleClick(this.props.value)}>
+        {this.props.value}
       </button>
     );
   }
