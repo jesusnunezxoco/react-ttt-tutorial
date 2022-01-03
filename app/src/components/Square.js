84f286165b56401b75bd705a0e59b5bd8854aefe
Jesus Nunez Xoconoxtle <jesusnunezxoco@gmail.com>
2022-01-03 14:40:50 -0600

    added state to fill in Xs in Square component

--- app/src/components/Square.js
+++ app/src/components/Square.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 
 // need to have props as a parameter for the functional component
 export default function Square(props) {
+    const [value, setValue] = useState("")
 
     const handleClick = () => {
       console.log("clickity click")
+      setValue("X")
     }
 
 
   // will access value through the `props` object
-  return <button className="square" onClick={handleClick}>{props.value}</button>;
+  return <button className="square" onClick={handleClick}>{value}</button>;
 }
