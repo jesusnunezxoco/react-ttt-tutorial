29ed0ffda1f6c994da232ec287d9713f1a3e9db7
Jesus Nunez Xoconoxtle <jesusnunezxoco@gmail.com>
2022-01-03 14:56:07 -0600

    shared state in Game board, passed down setter function

--- app/src/components/Board.js
+++ app/src/components/Board.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Square from './Square';
 
 export default function Board() {
+
+  let [squares, setSquares] = useState(Array(9).fill(null))
+  
+    const handleClick = (i) => {
+      console.log(squares)
+      let copy = squares.slice()
+      copy[i] = "X"
+      setSquares(copy)
+    }
+
+
   const renderSquare = (i) => {
-    return <Square value={i}/>;
+    return <Square value={squares[i]} handleClick={() => handleClick(i)}/>;
   };
 
   const status = "Next player: X";
--- app/src/components/Square.js
+++ app/src/components/Square.js
@@ -1,15 +1,9 @@
-import React, {useState} from "react";
+import React from "react";
 
 // need to have props as a parameter for the functional component
 export default function Square(props) {
-    const [value, setValue] = useState("")
-
-    const handleClick = () => {
-      console.log("clickity click")
-      setValue("X")
-    }
-
+    
 
   // will access value through the `props` object
-  return <button className="square" onClick={handleClick}>{value}</button>;
+  return <button className="square" onClick={() => props.handleClick(props.value)}>{props.value}</button>;
 }
