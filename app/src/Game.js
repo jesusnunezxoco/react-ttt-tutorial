3c3816e2d16b3740c6ffae86a39ca25fc043aceb
Jesus Nunez Xoconoxtle <jesusnunezxoco@gmail.com>
2022-01-03 15:40:24 -0600

    lifted state up to share history

--- app/src/Game.js
+++ app/src/Game.js
@@ -1,17 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import Board from "./components/Board";
 
 // Parent comopnent - equivalent to App.js in class
 export default function Game() {
+  // history is an array of objects that contain a property, squares
+  const [history, setHistory] = useState([{squares: Array(9).fill(null)}]);
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClick = (i) => {
+   
+    let current = history[history.length-1]
+    // make copy of current squares
+    let copy = current.squares.slice();
+    // don't handle click if square already filled
+    if (current.squares[i]) return;
+     // don't handle click if there's already a winner
+    if(calculateWinner(copy)) return;
+    
+    copy[i] = xIsNext ? "X" : "O";
+    setHistory(history.concat([{squares: copy}]))
+    setXIsNext(!xIsNext);
+  };
+
+  const current = history[history.length-1]
+  const winner = calculateWinner(current.squares);
+  let status
+  if (winner) {
+    status = `Winner: ${winner}`
+  } else {
+    status = `Next Player: ${xIsNext ? "X" : "O"}`
+  }
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board squares={current.squares} handleClick={(i) => handleClick(i)}/>
       </div>
       <div className="game-info">
-        <div>{/* status */}</div>
+        <div>{status}</div>
         <ol>{/* TODO */}</ol>
       </div>
     </div>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
--- app/src/components/Board.js
+++ app/src/components/Board.js
@@ -1,41 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import Square from "./Square";
 
-export default function Board() {
-  let [squares, setSquares] = useState(Array(9).fill(null));
-  let [xIsNext, setXIsNext] = useState(true);
-
-  const handleClick = (i) => {
-    console.log(squares);
-    let copy = squares.slice();
-
-    // don't handle click if square already filled
-    if (squares[i]) return;
-     // don't handle click if there's already a winner
-    if(calculateWinner(copy)) return;
-    
-
-    copy[i] = xIsNext ? "X" : "O";
-    setSquares(copy);
-    setXIsNext(!xIsNext);
-  };
-
+export default function Board({ squares, handleClick }) {
   const renderSquare = (i) => {
     return <Square value={squares[i]} handleClick={() => handleClick(i)} />;
   };
 
-  const winner = calculateWinner(squares);
-  let status
-
-  if (winner) {
-    status = `Winner: ${winner}`
-  } else {
-    status = `Next Player: ${xIsNext ? "X" : "O"}`
-  }
-
   return (
     <div>
-      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -53,24 +25,4 @@ export default function Board() {
       </div>
     </div>
   );
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
