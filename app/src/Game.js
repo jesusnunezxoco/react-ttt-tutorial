ab983bea467e8e5dbbd54f13f9eaac7432dba2f3
Jesus Nunez Xoconoxtle <jesusnunezxoco@gmail.com>
2022-01-03 16:12:00 -0600

    implemented history and time travel

--- app/src/Game.js
+++ app/src/Game.js
@@ -4,40 +4,57 @@ import Board from "./components/Board";
 // Parent comopnent - equivalent to App.js in class
 export default function Game() {
   // history is an array of objects that contain a property, squares
-  const [history, setHistory] = useState([{squares: Array(9).fill(null)}]);
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
   const [xIsNext, setXIsNext] = useState(true);
+  const [stepNumber, setStepNumber] = useState(0);
 
   const handleClick = (i) => {
-   
-    let current = history[history.length-1]
+    let h = history.slice(0, stepNumber + 1)
+    let current = h[h.length - 1];
     // make copy of current squares
     let copy = current.squares.slice();
     // don't handle click if square already filled
     if (current.squares[i]) return;
-     // don't handle click if there's already a winner
-    if(calculateWinner(copy)) return;
-    
+    // don't handle click if there's already a winner
+    if (calculateWinner(copy)) return;
+
     copy[i] = xIsNext ? "X" : "O";
-    setHistory(history.concat([{squares: copy}]))
+    setHistory(h.concat([{ squares: copy }]));
     setXIsNext(!xIsNext);
+    setStepNumber(h.length)
   };
 
-  const current = history[history.length-1]
-  const winner = calculateWinner(current.squares);
-  let status
-  if (winner) {
-    status = `Winner: ${winner}`
-  } else {
-    status = `Next Player: ${xIsNext ? "X" : "O"}`
+  const jumpTo = (step) => {
+    setStepNumber(step)
+    setXIsNext(step % 2 === 0)
   }
+
+  const current = history[stepNumber];
+  const winner = calculateWinner(current.squares);
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next Player: ${xIsNext ? "X" : "O"}`;
+  }
+
+  const moves = history.map((step, move) => {
+    const desc = move ? "Go to move #" + move : "Go to game start";
+    return (
+      <li key={move}>
+        {" "}
+        <button onClick={() => jumpTo(move)}>{desc}</button>{" "}
+      </li>
+    );
+  });
   return (
     <div className="game">
       <div className="game-board">
-        <Board squares={current.squares} handleClick={(i) => handleClick(i)}/>
+        <Board squares={current.squares} handleClick={(i) => handleClick(i)} />
       </div>
       <div className="game-info">
         <div>{status}</div>
-        <ol>{/* TODO */}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
